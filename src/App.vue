c7a91225c08f055fbd7ff7885c5c158840839165
Thais <thaisuckeman@yahoo.com.br>
2022-01-18 18:17:46 -0300

    Criando o componente News

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
     <HcodeHeader />
+    <HcodeSection />
     <HcodeFooter />
   </div>
 </template>
 
 <script>
 import HcodeHeader from "./components/HcodeHeader.vue";
-import HcodeFooter from "./components/HcodeFooter";
+import HcodeFooter from "./components/HcodeFooter.vue";
+import HcodeSection from "./components/HcodeSection.vue";
 
 export default {
   name: "App",
   components: {
     HcodeHeader,
     HcodeFooter,
+    HcodeSection,
   },
 };
 </script>
---
+++ src/components/HcodeSection.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <HcodeSectionBanner />
+  </div>
+</template>
+
+<script>
+import HcodeSectionBanner from "./HcodeSectionBanner.vue";
+export default {
+  components: {
+    HcodeSectionBanner,
+  },
+};
+</script>
+
+<style scoped>
+</style>
