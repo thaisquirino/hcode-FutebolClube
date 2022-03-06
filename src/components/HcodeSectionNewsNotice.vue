599460986876c1c4d2edeb7496df0eda436f9bf7
Thais <thaisuckeman@yahoo.com.br>
2022-03-06 12:42:30 -0300

    Praticando Criando validação para renderoização de um componente

--- src/components/HcodeSectionNewsNotice.vue
+++ src/components/HcodeSectionNewsNotice.vue
@@ -14,10 +14,6 @@
              </div>
          </div>
      </div>
-
-     
-
-
     </section>
 
 </template>
@@ -30,8 +26,17 @@ export default {
             return this.$store.getters.getNewsFromId(this.$route.params.idnotice);
         }
     },
-    mixins:[Utils]
-} 
+    mixins:[Utils],
+    beforeRouteEnter:(to, from, next) => {
+     
+        let params= to.params;
+
+        if(!parseInt(params.idnotice) > 0){
+        next('/');
+      }
+        next();
+    } 
+  }
 </script>
 <style scoped>
 h2{
